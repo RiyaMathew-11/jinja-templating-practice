44b97c1f3d4f40d80534b363e816d4bb5a6408bd
RiyaMathew-11 <riyapmathew2000@gmail.com>
2023-12-06 15:53:54 +0530

    refactor: added html rendering with loops

--- code/rendering-with-control-flow/script.py
+++ code/rendering-with-control-flow/script.py
@@ -1,18 +1,34 @@
 from jinja2 import Environment, FileSystemLoader
 
+
 names = [
     { 'name': 'John', 'age': 18},
     { 'name': 'Martha', 'age': 15},
     { 'name': 'Tim', 'age': 25},
 ]
-
 environment = Environment(loader=FileSystemLoader('code/rendering-with-control-flow/templates'))
-template = environment.get_template('message.txt')
 
-for name in names:
-    filename = 'code/rendering-with-control-flow/messages/' + name['name'] + '.txt'
-    content = template.render(name)
+# Rendering Text Messages with if statements
+
+# template = environment.get_template('message.txt')
+
+# for name in names:
+#     filename = 'code/rendering-with-control-flow/messages/' + name['name'] + '.txt'
+#     content = template.render(name)
     
-    with open(filename, mode = 'w', encoding = 'utf-8') as messagefile:
-        messagefile.write(content)
-        print('Wrote', filename)
+#     with open(filename, mode = 'w', encoding = 'utf-8') as messagefile:
+#         messagefile.write(content)
+#         print('Wrote', filename)
+        
+        
+# Rendering HTML with loops
+
+render_file = "code/rendering-with-control-flow/messages/age_data_render.html"
+render_template = environment.get_template('age_data.html')
+
+context = { "names": names }
+
+with open(render_file, mode = 'w', encoding = 'utf-8') as render_file:
+    render_file.write(render_template.render(context))
+    print('Wrote render file for', render_template)
+
